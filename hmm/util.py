e25192d4fa889aaa9a657d71ade84da48bea23ca
Sunset <s2821d3721@gmail.com>
2017-02-23 19:43:26 +0800

    Add LogNum

---
+++ hmm/util.py
@@ -0,0 +1,37 @@
+import math
+
+ZERO = 1e-12
+LOG_ZERO = math.log(1e-12)
+
+class LogNum:
+    def __init__(self, val=-1):
+        self.val = math.log(val if val > ZERO else ZERO) if val >= 0 else -1
+
+    def __float__(self):
+        return math.exp(self.val)
+
+    def __str__(self):
+        return str(self.__float__())
+
+    def __mul__(self, other):
+        tmp = LogNum()
+        tmp.val = self.val + other.val
+        return tmp
+
+    def __truediv__(self, other):
+        tmp = LogNum()
+        tmp.val = self.val - other.val
+        return tmp
+
+    def __add__(self, other):
+        tmp = LogNum()
+        a = max(self.val, other.val)
+        b = min(self.val, other.val)
+        tmp.val = a + math.log(1.0 + math.exp(b-a))
+        return tmp
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __gt__(self, other):
+        return self.val > other.val;
