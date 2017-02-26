99a3fa6509dc8323d01dab1439c75f4072bf8bd2
Sunset <s2821d3721@gmail.com>
2017-02-26 11:11:26 +0800

    Add log_add log_sum

--- hmm/util.py
+++ hmm/util.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from functools import reduce
 
 EPS = 1e-4
 ZERO = 1e-12
@@ -18,35 +19,10 @@ def log_mul(*arrays):
 def log_div(*arrays):
     return arrays[0] - log_mul(*arrays[1:])
 
-class LogNum:
-    def __init__(self, val=-1):
-        self.val = math.log(val if val > ZERO else ZERO) if val >= 0 else -1
+def log_add(*arrays):
+    a = max(arrays[0], arrays[1])
+    b = min(arrays[0], arrays[1])
+    return a + math.log(1.0 + math.exp(b-a))
 
-    def __float__(self):
-        return math.exp(self.val)
-
-    def __str__(self):
-        return str(self.__float__())
-
-    def __mul__(self, other):
-        tmp = LogNum()
-        tmp.val = self.val + other.val
-        return tmp
-
-    def __truediv__(self, other):
-        tmp = LogNum()
-        tmp.val = self.val - other.val
-        return tmp
-
-    def __add__(self, other):
-        tmp = LogNum()
-        a = max(self.val, other.val)
-        b = min(self.val, other.val)
-        tmp.val = a + math.log(1.0 + math.exp(b-a))
-        return tmp
-
-    def __lt__(self, other):
-        return self.val < other.val
-
-    def __gt__(self, other):
-        return self.val > other.val;
+def log_sum(*arrays):
+    return reduce(lambda x,y: log_add(x, y), arrays)
