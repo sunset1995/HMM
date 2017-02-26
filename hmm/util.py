8bbe903ae6c655de176165d46b60285b86bef073
Sunset <s2821d3721@gmail.com>
2017-02-26 11:44:50 +0800

    [util] vector add/mul

--- hmm/util.py
+++ hmm/util.py
@@ -13,11 +13,16 @@ def normalize1d(vec):
     return vec / vec.sum()
 
 def log_mul(*arrays):
-    print(arrays)
     return np.array(arrays).sum(axis=0)
+
+def log_vec_mul(*vec_arrays):
+    return np.sum(vec_arrays, axis=0)
 
 def log_div(*arrays):
     return arrays[0] - log_mul(*arrays[1:])
+
+def log_vec_div(*vec_arrays):
+    return vec_arrays[0] - log_vec_mul(*vec_arrays[1:])
 
 def log_add(*arrays):
     a = max(arrays[0], arrays[1])
