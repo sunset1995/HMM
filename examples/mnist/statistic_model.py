ff5ae4075be4150453cc4e2a67a4b40fe84b8d76
Sunset <s2821d3721@gmail.com>
2017-03-15 21:29:50 +0800

    [examples] Update statistic method

--- examples/mnist/statistic_model.py
+++ examples/mnist/statistic_model.py
@@ -10,9 +10,27 @@ cnt = np.full((N-1, M), 0, dtype=np.int32)
 for i in range(train_x.shape[0]):
     x = train_x[i].reshape((28, 28))
     y = int(train_y[i])
+
+    s_pos = 0
+    e_pos = 27
+    while s_pos < e_pos and x.T[s_pos].sum() < 10:
+    	s_pos += 1
+    while e_pos > s_pos and x.T[e_pos].sum() < 10:
+    	e_pos -= 1
+
+    m_pos = (s_pos + e_pos) // 2
+    ms_pos = (s_pos + m_pos) // 2
+    me_pos = (e_pos + m_pos) // 2
+    rng = (
+    	range(s_pos, ms_pos),
+    	range(ms_pos, m_pos),
+    	range(m_pos, me_pos),
+    	range(me_pos, e_pos+1),
+    )
+
     for j in range(4):
-        for k in range(7):
-            obs = data_proc.encode_col_k_means(x.T[j*7+k])
+        for k in rng[j]:
+            obs = data_proc.encode_col_k_means(x.T[k])
             cnt[j*10+y][obs] += 1
 
 total = cnt.sum(axis=1)
